0264381e359a7303c2cbae208758c22d76a64c5e
Nikas <solarisocean@gmail.com>
2020-12-21 01:18:48 +0200

    added black leds and reactive brightness

--- muviled_music_visualizing_leds.ino
+++ muviled_music_visualizing_leds.ino
@@ -1,6 +1,7 @@
 /*
-LED audio visualizer (by Nikas M.)
+MuViLED - Music Visualizing LEDS (by Nikas M.)
 num_leds - the total number of LEDs you are going to use.
+pin - pin to control the LED strip
 */
 
 #include <FastLED.h>
@@ -12,20 +13,23 @@ CRGB leds[num_leds];
 
 void setup()
 {
-
+//  Safe startup delay
+  delay(3000);
+//  Serial.begin(9600);
   FastLED.addLeds<WS2812B, pin, GRB>(leds, num_leds);
 }
 
 void loop()
 {
-
   // Potentiometer that limits the voltage range from the audio input and so changes the sensitivity of the audio input.
   int sensitivity = (analogRead(A2));
-  sensitivity = map(sensitivity, 0, 1023, 700, 280);
+  sensitivity = map(sensitivity, 0, 1023, 1000, 260);
 
-  // Audio input from MAX9414 or similar. Carier DC in my case gives an analogRead of around 240 so I reduce this value in map function to measure the input from zero.
+  // Audio input from MAX9414 or similar. Carrier DC in my case gives an analogRead of around 240 so I reduce this value in map function to measure the input from zero.
   int audioIn = analogRead(A0);
-  audioIn = (map(audioIn, 240, sensitivity, 0, 255));
+  
+  audioIn = (map(audioIn, 250, sensitivity, 0, 255));
+  
 
   // Pot controlling brightness
   int brightness = analogRead(A3);
@@ -39,7 +43,7 @@ void loop()
   int userDelay = analogRead(A5);
   userDelay = map(userDelay, 0, 1023, 10, 60);
 
-  if (audioIn > 0)
+  if (audioIn > 10)
   {
     int y = audioIn + userHue;
 
@@ -48,11 +52,16 @@ void loop()
     {
       y = y % 255;
     }
+//    Serial.println(y);
+    leds[(num_leds / 2)] = CHSV(y, 255, y * 2);
 
-    leds[(num_leds / 2)] = CHSV(y, 255, brightness);
-
-    // Comment the line below to disable the blur and make the visualizer more reactive.
-    blur1d(leds, num_leds, 5);
+   
+    
+  } else {
+    leds[(num_leds / 2)] = CRGB::Black;
+  }
+   // Comment the line below to disable the blur and make the visualizer more reactive.
+    //    blur1d(leds, num_leds, 5);
 
     for (int z = num_leds; z > (num_leds / 2); z--)
     {
@@ -62,7 +71,6 @@ void loop()
     {
       leds[z] = leds[z + 1];
     }
-    FastLED.show();
+  FastLED.show();
     delay(userDelay);
-  }
 }
